217ea9d6e99d49df231419d36c2edd2ee23a13a0
Caeman Toombs <toombs.caeman@gmail.com>
2023-10-23 16:57:58 -0400

    vectorized updates, guess and backtrack

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 a very rough sudoku solver in python
+
+This solver represents the board as an array 9-bit bitfields, representing the *potential values* that could be contained in each cell.
+
+Row, column, and block constraints are propagated whenever cells are collapsed to a single value by reducing the potential field of adjacent cells, which may trigger further propagation.
+
+When constraint propagation ends, the solver can guess the remaining cells, backtracking whenever a guess results in a contradiction.
+Guessing is technically unnecessary as puzzles are supposed to have a single solution, however more complicated inference would be necessary. Guessing is succinct and sufficiently fast.
+
+# Future Ideas
+* re-write in various languages and test relative performance
+* write fully logical implementation (no guessing/backtracking), perf test
+    *
+* [generate puzzles with unique solutions](https://stackoverflow.com/questions/6924216/how-to-generate-sudoku-boards-with-unique-solutions)
+* interactive TUI
+    * [example game](https://www.linuxlinks.com/nudoku/)
+    * [termcolor](https://pypi.org/project/termcolor/)
+* other puzzle solvers
+    * allow varied grid size/shape
+    * [kakuro](https://en.wikipedia.org/wiki/Kakuro)
+# reference
+* https://sandiway.arizona.edu/sudoku/index.html#heuristics
--- sudoku.py
+++ sudoku.py
@@ -1,59 +1,10 @@
 import numpy as np
-from dataclasses import dataclass, field
-# https://sandiway.arizona.edu/sudoku/index.html#heuristics
 
-@dataclass
-class cell:
-    grid: 'np.array' = None
-    x: int = 0
-    y: int = 0
-    potentials: set = field(default_factory=lambda:set(range(1,10)))
-    def __hash__(self):
-        return 9 * self.y + self.x
-    # nditer / flatiter
-    @property
-    def blk(self):
-        x = self.x//3 * 3
-        y = self.y//3 * 3
-        return self.grid[x:x+2,y:y+2].flatten()
-    @property
-    def row(self):
-        return self.grid[self.x,:]
-    @property
-    def col(self):
-        return self.grid[:,self.y]
-    @property
-    def val(self):
-        return self.potentials
+class Contradiction(Exception):
+    """Thrown when assigning a value results in another cell having no possible values"""
 
-    #@val.setter
-    def assign(self, val):
-        self.potentials = set((val,))
-        self.update_neighbors()
-
-    def reduce(self, nval):
-        if len(self.potentials) == 1:
-            return
-        self.potentials -= nval
-        # it's a contradiction to have no options
-        if not self.val:
-            raise SyntaxError
-
-        if len(self.potentials) == 1:
-            self.update_neighbors()
-
-    def update_neighbors(self):
-        n = set((*self.blk, *self.row, *self.col))
-        for c in n:
-            if c != self:
-                c.reduce(self.val)
-
-    def __str__(self):
-        if len(self.potentials) > 1:
-            return ' '
-        return str(next(iter(self.potentials)))
-
-class grid:
+class sudoku:
+    shape = (9,9)
     fmt = """*-----------------*
 |{} {} {}|{} {} {}|{} {} {}|
 |{} {} {}|{} {} {}|{} {} {}|
@@ -68,33 +19,97 @@ class grid:
 |{} {} {}|{} {} {}|{} {} {}|
 *-----------------*"""
     def __init__(self, values = None) -> None:
-        shape = (9,9)
-        self.grid = np.array([cell() for _ in range(81)]).reshape(shape)
-        for idx in np.ndindex(shape):
-            e = self.grid[idx]
-            e.x, e.y, e.grid = *idx, self.grid
+        # represent values 1..9 as a bitfield. Fill grid with full potential 0b111111111
+        self.grid = np.full(self.shape, (1 << 9) - 1, dtype=np.uint16)
         if values:
             self.load(values)
+
     def load(self, values):
-        for (x,y), e in np.ndenumerate(np.array(values).reshape((9,9))):
-            if e:
-                self.grid[x,y].assign(e)
+        for idx, value in np.ndenumerate(np.array(values).reshape(self.shape)):
+            if value:
+                self[idx] = value
+
+    def guess(self):
+        checkpoint = self.grid.copy()
+        # find first ambiguous index
+        for idx, bits in np.ndenumerate(self.grid):
+            if bits.bit_count() == 1:
+                continue
+            # loop through all potential values at idx
+            value = 1
+            while bits:
+                if not 1 & bits:
+                    value, bits = value + 1, bits >> 1
+                    continue
+                try:
+                    self[idx] = value
+                    self.guess()
+                    break
+                except Contradiction:
+                    self.grid[:,:] = checkpoint[:,:]
+                    value, bits = value + 1, bits >> 1
+            else:
+                # we've tried all possible values at idx and all lead to contradictions.
+                # a previous guess was wrong. we need to backtrack
+                raise Contradiction
+
+    def __getitem__(self, position):
+        # convert bitfield into integer, return 0 if the value isn't yet determined.
+        b = int(self.grid[position])
+        return b.bit_length() if b.bit_count() == 1 else 0
+
+    def __setitem__(self, position, value):
+        # convert integer into bitfield
+        bit = 1 << (value - 1)
+
+        # all places where propagation may happen
+        # this considers only direct row, column, and block constraints
+        mask = np.zeros(self.shape, dtype=np.uint16)
+        mask[position[0],:] = 1
+        mask[:,position[1]] = 1
+        x0, y0 = position[0] - (position[0] % 3), position[1] - (position[1] % 3)
+        mask[x0:x0+3,y0:y0+3] = 1
+        mask[position] = 0
+
+        # where propagation actually will happen
+        prop = (self.grid & (mask*bit))
+
+        # make changes
+        self.grid[position] = bit
+        self.grid ^= prop
+
+        # if any bitfield is 0, that cell can't contain any values
+        if np.any(self.grid == 0):
+            raise Contradiction
+
+        # propagate to indexes that changed and are now final values
+        for idx in zip(*prop.nonzero()):
+            if self[idx]:
+                self[idx] = self[idx]
 
     def __str__(self):
-        return self.fmt.format(*self.grid.flatten())
-
-    def check(self, solution):
         return self.fmt.format(*(
-            ' ' if s == a else 'X'
-            for s, a in zip((next(iter(c.val)) for c in self.grid.flatten()), solution)
+            int(b).bit_length() if b.bit_count() == 1 else ' ' for b in self.grid.flatten()
             ))
 
-    @property
-    def cpot(self):
-        return self.fmt.format(*(len(c.val) for c in self.grid.flatten()))
+    def diff(self, solution):
+        match = [self[idx] == a
+            for (idx, _), a in zip(np.ndenumerate(self.grid), solution)
+                 ]
+        return False if all(match) else self.fmt.format(*(['X', ' '][v] for v in match))
 
+blank =[0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0]
 # https://sandiway.arizona.edu/sudoku/examples.html
-#      |     |     |     |
+# Arizona Daily Wildcat: Tuesday, Jan 17th 2006
+# guessing not necessary
 easy1 =[0,0,0,2,6,0,7,0,1,
         6,8,0,0,7,0,0,9,0,
         1,9,0,0,0,4,5,0,0,
@@ -114,8 +129,31 @@ sol1 = [4,3,5,2,6,9,7,8,1,
         2,4,8,9,5,7,1,3,6,
         7,6,3,4,1,8,2,5,9]
 
-g = grid(easy1)
+# Vegard Hanssen puzzle 2155141
+# guessing also not technically necessary, but the simple propagation implemented here is insufficient
+hard2 =[0,0,0,6,0,0,4,0,0,
+        7,0,0,0,0,3,6,0,0,
+        0,0,0,0,9,1,0,8,0,
+        0,0,0,0,0,0,0,0,0,
+        0,5,0,1,8,0,0,0,3,
+        0,0,0,3,0,6,0,4,5,
+        0,4,0,2,0,0,0,6,0,
+        9,0,3,0,0,0,0,0,0,
+        0,2,0,0,0,0,1,0,0]
+sol2 = [5,8,1,6,7,2,4,3,9,
+        7,9,2,8,4,3,6,5,1,
+        3,6,4,5,9,1,7,8,2,
+        4,3,8,9,5,7,2,1,6,
+        2,5,6,1,8,4,9,7,3,
+        1,7,9,3,2,6,8,4,5,
+        8,4,5,2,1,9,3,6,7,
+        9,1,3,7,6,8,5,2,4,
+        6,2,7,4,3,5,1,9,8]
+
+g = sudoku(easy1)
 print(g)
-#print(g.cpot)
-print(g.fmt.format(*sol1))
-print(g.check(sol1))
+print(g.diff(sol1) or 'ok')
+g = sudoku(hard2)
+g.guess()
+print(g)
+print(g.diff(sol2) or 'ok')
